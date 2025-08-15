a7f4762289f5dca10dcd1adecc77266435d499ec
Nelly Aviles <143915388+Sodi0@users.noreply.github.com>
2025-08-14 22:59:48 -0400

    feat: add main Spring Boot application with CommandLineRunner to launch Principal menu

--- src/main/java/com/aluracursos/challengeliteralura/ChallengeLiteraluraApplication.java
+++ src/main/java/com/aluracursos/challengeliteralura/ChallengeLiteraluraApplication.java
@@ -1,13 +1,27 @@
 package com.aluracursos.challengeliteralura;
 
+import com.aluracursos.challengeliteralura.principal.Principal;
+import com.aluracursos.challengeliteralura.repository.IAuthorRepository;
+import com.aluracursos.challengeliteralura.repository.IBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeLiteraluraApplication {
+public class ChallengeLiteraluraApplication implements CommandLineRunner {
+    @Autowired
+    private IBookRepository bookRepository;
+    @Autowired
+    private IAuthorRepository authorRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChallengeLiteraluraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeLiteraluraApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(bookRepository, authorRepository);
+        principal.mostrarMenu();
+    }
 }
